64c770216613e23e7887ded865598f665f9fb700
Ganeshraaj1 <ssganeshraaj@gmail.com>
2022-03-09 19:08:01 +0530

    responsive NFinished

--- src/scroll.js
+++ src/scroll.js
@@ -2,3 +2,19 @@ window.addEventListener('scroll', function(){
     var scroll = this.document.querySelector(".gotopbtn");
     scroll.classList.toggle("visible" , window.scrollY>510)
 })
+
+
+
+// to Find Overflow mistakes  use it in console
+
+
+// var docWidth = document.documentElement.offsetWidth;
+
+// [].forEach.call(
+//   document.querySelectorAll('*'),
+//   function(el) {
+//     if (el.offsetWidth > docWidth) {
+//       console.log(el);
+//     }
+//   }
+// );
